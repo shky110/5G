9312855a7911fce9a39b48a11f8584faf906f43f
shky110 <jbltech1901@gmail.com>
2025-03-26 18:26:06 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include("config/db.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>5G Investment Platform</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to 5G Investment Platform</h1>
+    <a href="users/register.php">Register</a> | <a href="users/login.php">Login</a>
+</body>
+</html>
