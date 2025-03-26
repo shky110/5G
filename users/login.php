a3411e5e275cfab98ae0b3f7ff2cce09f1f8894d
shky110 <jbltech1901@gmail.com>
2025-03-26 18:27:47 +0500

    Create login.php

---
+++ users/login.php
@@ -0,0 +1,29 @@
+<?php
+include("../config/db.php");
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        $user = mysqli_fetch_assoc($result);
+        if (password_verify($password, $user['password'])) {
+            $_SESSION['user'] = $username;
+            header("Location: ../dashboard.php");
+        } else {
+            echo "Incorrect password.";
+        }
+    } else {
+        echo "User not found.";
+    }
+}
+?>
+<form method="post">
+    <input type="text" name="username" placeholder="Username" required>
+    <input type="password" name="password" placeholder="Password" required>
+    <button type="submit">Login</button>
+</form>
